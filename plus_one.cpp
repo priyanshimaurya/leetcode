2885941ca077da557f08e00f0dfd7eb786691df6
jyoti maurya <jyotiangel1993@gmail.com>
2024-02-19 12:03:15 +0530

    plus one  added

---
+++ plus_one.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,arr[6]={2,4,1,5,7,10};
+    for(i=0;i<6;i++){
+        if(arr[i]==arr[i]-1);
+    }
+    cout<<arr[i-1]+1;
+    return 0;
+}
